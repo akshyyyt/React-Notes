2fd85640b461b2c1c6cf03d2f17a7a64be1b7972
akshyyyt <akshittomar5@gmail.com>
2026-04-20 22:41:33 +0530

    docs: keys and list

--- 02_todo/src/App.jsx
+++ 02_todo/src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from 'react'
 
 function App() {
-  const [todo, setTodo] = useState([{
-    title: "Go to Gym",
-    status: false 
-  }])
+  const [todo, setTodo] = useState([
+    {
+      title: "Go to Gym",
+      status: false 
+    },
+    {
+      title: "Study",
+      status: true
+    }
+  ])
 
   function appendTodo(){
-    let newArray = [...todo]
-    newArray.push({
-      title: "React",
-      status: false
-    })
-    setTodo(newArray) 
+    setTodo([...todo, {title: "React", status: false}])
+  }
+
+  function Map(){
+    return (
+      todo.map((todo, index) => <div key={index}>{todo.title}, {todo.status ? "Done" : "Not Done"}</div>) // Here we give key to it so react can distinguish between the elements
+    )
   }
 
   return (
     <>
       <button onClick={appendTodo}>Add Todo</button>
-      {JSON.stringify(todo)}
+      <Map/>
     </>
   )
 }
