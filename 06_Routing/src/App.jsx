5cc9b9bd19dc258584d21ee99270f570cc335c0f
akshyyyt <akshittomar5@gmail.com>
2026-04-21 09:27:07 +0530

    docs: Routing

---
+++ 06_Routing/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+// Single Page Application (SPA). all data on single HTML page unlike orthodox way of creating new html page for everything
+
+// Now we have 2 Ways... Lazy Loading ie fetch request again when use go on that page to get the required HTML.
+// Another is all the data on the first fetch itself and then use it accordingly. (Eager Loading)
+// But in both case the application remains single page. Only the difference of optimizations
+
+ 
+
+export default App
